413bcf5a22627397a11d5df9abc17c5f08f21ae6
viveoff <vlad.shorodok14@gmail.com>
2024-01-15 18:11:48 +0200

    Initial commit

---
+++ Curs/Curs6/problem2/ex2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#define N 20
+int ex2() {
+    int n,i, nr_par = 0;
+    int vect[N];
+
+    do {
+        printf("\n Introduceti numarul el:  ");
+        scanf("%d", &n);
+    } while (n<=0 || n>N);
+    for(i=0; i<n; i++)
+    {
+        printf("vect[%d]=", i);
+        scanf("%d", &vect[i]);
+    }
+
+    printf("Elementele pare de pe pozititiile impare sunt: \n");
+    for(i=1; i<N; i +=2)
+        if(vect[i] % 2 == 0)
+        {
+            printf("\t %d", vect[i]);
+            nr_par++;
+        }
+    printf("\n Nr de el de pe pozitii impare este: %d\n", nr_par);
+    return 1;
+}
---
+++ Curs/Curs6/problem2/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    auto int ex2()
+    ex2();
+    printf("Hello, World!\n");
+    return 0;
+}
